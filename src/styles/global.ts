731f702c9bc29407325b7edb1e3fce0df08de6da
Matheus Barbosa Miranda <mxtheusbm@gmail.com>
2024-07-31 15:07:26 -0300

    setup

---
+++ src/styles/global.ts
@@ -0,0 +1,49 @@
+'use client'
+
+import { createGlobalStyle, css } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-display: swap;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 300;
+    src: url('/fonts/poppins-v21-latin-300.woff2') format('woff2'); 
+  }
+
+  @font-face {
+    font-display: swap; 
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    src: url('/fonts/poppins-v21-latin-regular.woff2') format('woff2'); 
+  }
+
+  @font-face {
+    font-display: swap; 
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    src: url('/fonts/poppins-v21-latin-600.woff2') format('woff2'); 
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
+`
+
+export default GlobalStyles
