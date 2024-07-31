1b77ad1a209214b54f91a19854273f4dc38da006
Matheus Barbosa Miranda <mxtheusbm@gmail.com>
2024-07-31 19:47:39 -0300

    feat(heading): create heading component

---
+++ src/components/Heading/stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Heading, { HeadingProps } from '.'
+
+export default {
+  title: 'Heading',
+  component: Heading,
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
+} as Meta
+
+export const Default: StoryObj<HeadingProps> = {
+  render: (args) => <Heading {...args} />
+}
+
+Default.args = {
+  children: 'Most Populars',
+  color: 'black'
+}
